a280ebb3d39fa374dc99d56fa8a778a7cad83be9
moormaster <moormaster@gmx.net>
2019-10-10 21:02:34 +0200

    refactoring: simplified winworldpc-get-download-url.sh usage

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 WINWORLDPCMIRRORNAME="Ricky"
 
 MSDOS622_ARCHIVE="Microsoft MS-DOS 6.22 Plus Enhanced Tools (3.5).7z"
-MSDOS622_URL=`./winworldpc-get-download-url.sh "https://winworldpc.com" "/product/ms-dos/622" "Microsoft MS-DOS 6.22 Plus Enhanced Tools (3.5).7z" "${WINWORLDPCMIRRORNAME}"`
+MSDOS622_URL=`./winworldpc-get-download-url.sh "https://winworldpc.com/product/ms-dos/622" "Microsoft MS-DOS 6.22 Plus Enhanced Tools (3.5).7z" "${WINWORLDPCMIRRORNAME}"`
 
 W311FWG_ARCHIVE="Microsoft Windows for Workgroups 3.11 (OEM) (3.5-1.44mb).7z"
-W311FWG_URL=`./winworldpc-get-download-url.sh "https://winworldpc.com" "/product/windows-3/wfw-311" "Microsoft Windows for Workgroups 3.11 (OEM) (3.5-1.44mb).7z" "${WINWORLDPCMIRRORNAME}"`
+W311FWG_URL=`./winworldpc-get-download-url.sh "https://winworldpc.com/product/windows-3/wfw-311" "Microsoft Windows for Workgroups 3.11 (OEM) (3.5-1.44mb).7z" "${WINWORLDPCMIRRORNAME}"`
 
 TCPIP_ARCHIVE="Microsoft TCP-IP-32 For Windows 3.1 (3.5).7z"
-TCPIP_URL=`./winworldpc-get-download-url.sh "https://winworldpc.com" "/product/microsoft-tcp-ip-32/tcpip-32-3-11b" "Microsoft TCP-IP-32 For Windows 3.1 (3.5).7z" "${WINWORLDPCMIRRORNAME}"`
+TCPIP_URL=`./winworldpc-get-download-url.sh "https://winworldpc.com/product/microsoft-tcp-ip-32/tcpip-32-3-11b" "Microsoft TCP-IP-32 For Windows 3.1 (3.5).7z" "${WINWORLDPCMIRRORNAME}"`
 
 CIRRUS_ARCHIVE="win_5446.zip"
 CIRRUS_URL="http://www.claunia.com/qemu/drivers/win_5446.zip"
--- winworldpc-get-download-url.sh
+++ winworldpc-get-download-url.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
+getbaseurl() {
+	url="$1"
+
+	echo "$url" | grep -o "\(https\?:\/\/[^\/]\+\)\/" | sed "s/\(https\?:\/\/[^\/]\+\)\//\1/"
+}
+
+geturlpath() {
+	url="$1"
+
+	echo "$url" | grep -o "\(https\?:\/\/[^\/]\+\)\(\/.*\)$" | sed "s/\(https\?:\/\/[^\/]\+\)\(\/.*\)$/\2/"	
+}
+
 winworldpc-get-mirrorlist-url() {
-	baseurl="$1"
-	urlpath="$2"
-	filename="$3"
+	url="$1"
+	filename="$2"
+
+	baseurl="$( getbaseurl "$url" )"
+	urlpath="$( geturlpath "$url" )"
+
 
 	echo -n "$baseurl"
 	curl "$baseurl$urlpath" | grep -o "href=\"\([^\"]\+\)\" title=\"${filename}\"" | sed "s/href=\"\([^\"]\+\)\" title=\"$filename\"/\1/"
 }
 
 winworldpc-get-download-url() {
-	baseurl="$1"
-	urlpath="$2"
-	filename="$3"
+	url="$1"
+	filename="$2"
+	mirrorname="$3"
 
-	mirrorname="$4"
+	baseurl="$( getbaseurl "$url" )"
+	urlpath="$( geturlpath "$url" )"
 
-	mirrorsurl="$( winworldpc-get-mirrorlist-url "$@" )"
+	mirrorsurl="$( winworldpc-get-mirrorlist-url "$url" "$filename" )"
 
 	echo -n "$baseurl"
 	curl "$mirrorsurl" | grep -o "<a href=\"\([^\"]\+\)\">$mirrorname</a>" | sed "s/<a href=\"\([^\"]\+\)\">$mirrorname<\/a>/\1/"
 }
 
 usage() {
-	echo "$0 <baseurl> <urlpath> <filename> <mirrorname>"
+	echo "$0 <url> <filename> <mirrorname>"
 	echo "example: $0 \"https://winworldpc.com\" \"/product/ms-dos/622\" \"Microsoft MS-DOS 6.22 Plus Enhanced Tools (3.5).7z\" \"Ricky\""
 }
 
-if [ $# -lt 4 ]
+if [ $# -lt 3 ]
 then
 	usage
 	exit
 fi
 
-winworldpc-get-download-url "$@"
+winworldpc-get-download-url "$@" 2> /dev/null
 
