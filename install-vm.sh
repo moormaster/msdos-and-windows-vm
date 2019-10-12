31017ccf4961f1ebf2043f4cb04883bbef0152bf
Andre Herbst <moormaster@gmx.net>
2019-10-12 17:42:43 +0200

    install win 3.11 for workgroups

--- install-vm.sh
+++ install-vm.sh
@@ -3,16 +3,19 @@
 . lib-qemu.sh
 . lib-install-dos-on-qemu.sh
 . lib-install-oak-cdromdriver-on-qemu.sh
+. lib-install-w311fwg-on-qemu.sh
 
 install-vm() {
-	imagefile="$1"
+	hddimage="$1"
+	isoimage="$2"
 
-	shift 1
+	shift 2
 	qemuargs=("$@")
 	
 	QEMU_PIPE=$( qemu-pipe-init )
 
-	qemu-system-i386 -enable-kvm -hda "${imagefile}" -fda "" -monitor "pipe:${QEMU_PIPE}" "${qemuargs[@]}" &
+	echo "qemu-system-i386 -enable-kvm -hda "${hddimage}" -fda "" -cdrom "" -monitor "pipe:${QEMU_PIPE}" "${qemuargs[@]}" &"
+	qemu-system-i386 -enable-kvm -hda "${hddimage}" -fda "" -cdrom "" -monitor "pipe:${QEMU_PIPE}" "${qemuargs[@]}" &
 
 	# wait for qemu to initialize
 	sleep 5
@@ -23,6 +26,9 @@ install-vm() {
 	echo installing cdrom driver...
 	install-oak-cdromdriver-on-qemu Win98BootDisk.img
 
+	echo installing windows 3.11 for workgroups
+	install-w311fwg-on-qemu "$isoimage"
+
 	qemu-send "quit"
 	# wait for qemu to close
 	wait
@@ -30,13 +36,27 @@ install-vm() {
 	qemu-pipe-destroy ${QEMU_PIPE}
 }
 
-usage() {
-	echo "$0 <HardDisk image>" 2>&1
+install-vm-usage() {
+	echo "$0 <HardDisk image> <Windows 3.11 install iso>" 2>&1
 }
 
 if [ $# -lt 1 ]
 then
-	usage
+	install-vm-usage
+	exit
+fi
+
+if ! [ -f "$1" ]
+then
+	echo "file not found: $1" 1>&2
+	install-vm-usage
+	exit
+fi
+
+if ! [ -f "$2" ]
+then
+	echo "file not found: $2" 1>&2
+	install-vm-usage
 	exit
 fi
 
