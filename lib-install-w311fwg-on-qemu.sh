8d1448d6608823bdacd89d184e6050a69a53c153
Andre Herbst <moormaster@gmx.net>
2019-10-16 01:10:07 +0200

    corrected sleep times

--- lib-install-w311fwg-on-qemu.sh
+++ lib-install-w311fwg-on-qemu.sh
@@ -168,7 +168,7 @@ install-w311fwg-on-qemu() {
 	bogomips-sleep 15
 	echo "confirming installation directory..."
 	qemu-send-key "ret"
-	bogomips-sleep 70
+	bogomips-sleep 85
 	echo "skipping customization file..."
 	qemu-send-key "tab"
 	qemu-send-key "tab"
