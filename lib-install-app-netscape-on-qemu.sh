c3c397e7cc44c6012c32c1536bca3f354e149862
moormaster <moormaster@gmx.net>
2026-04-03 23:53:40 +0200

    fix(lib-install-app-netscape-on-qemu.sh): increase installation sleep times

--- lib-install-app-netscape-on-qemu.sh
+++ lib-install-app-netscape-on-qemu.sh
@@ -20,39 +20,39 @@ install-app-netscape-on-qemu() {
 	qemu-change-cdrom "$isoimage"
 	echo "running windows..."
 	qemu-send-line-de "c:\\windows\\win.com"
-	bogomips-sleep 24
+	bogomips-sleep 28
 	echo "installing netscape..."
 	qemu-send-key "alt-f"
 	qemu-send-key "r"
 	qemu-send-line-de "d:\\apps\\netscape\\netscape.exe"
-	bogomips-sleep 240
+	bogomips-sleep 250
 	echo "confirming welcome message..."
 	qemu-send-key "ret"
-	bogomips-sleep 1.2
-	echo "confirming welcome license..."
+	bogomips-sleep 2.0
+	echo "confirming license..."
 	qemu-send-key "ret"
-	bogomips-sleep 1.2
+	bogomips-sleep 2.0
 	echo "confirming installation directory..."
 	qemu-send-key "ret"
-	bogomips-sleep 1.2
+	bogomips-sleep 2.0
 	echo "confirming installation directory creation..."
 	qemu-send-key "ret"
-	bogomips-sleep 2.4
+	bogomips-sleep 10
 	echo "confirming video for windows message..."
 	qemu-send-key "ret"
-	bogomips-sleep 12
+	bogomips-sleep 18
 	echo "confirming windows group name..."
 	qemu-send-key "ret"
-	bogomips-sleep 1.2
+	bogomips-sleep 2.0
 	echo "confirming installation begin..."
 	qemu-send-key "ret"
-	bogomips-sleep 258
+	bogomips-sleep 505
 	echo "denying to view readme..."
 	qemu-send-key "n"
-	bogomips-sleep 5
+	bogomips-sleep 2.0
 	echo "rebooting..."
 	qemu-send-key "ret"
-	bogomips-sleep 1.2
+	bogomips-sleep 5.0
 	qemu-send-key "ret"
 	bogomips-sleep 28
 }
